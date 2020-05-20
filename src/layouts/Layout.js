fcbad47352cd3240988abf3e3293c8e87cd2ea3b
sergio.gordo <sergio@meta-site.com>
2020-05-21 00:39:56 +0200

    Header working responsive Trusted partners working responsive

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "../components/Header/Header"
+import HeaderSection from "../components/HeaderSection/HeaderSection"
 import Helmet from "react-helmet"
 import useSiteMetadata from "../static_queries/useSiteMetadata"
 import layoutStyles from "./layout.module.scss"
@@ -8,23 +8,25 @@ import Footer from "../components/FooterSection/Footer"
 export default function Layout(props) {
   const { metaTitle, metaDescription, title } = useSiteMetadata()
   return (
-    <section
-      className={`${layoutStyles.layout} ${
-        props.page === "info" &&
-        layoutStyles.info_page}`}
-      style={{
-        backgroundColor: props.bgColor,
-      }}
-    >
-      <Helmet>
-        <html lang="en" />
-        <title>{metaTitle}</title>
-        <meta name="description" content={metaDescription} />
-      </Helmet>
-      <Header page={props.page} title={title} />
-      <div className={layoutStyles.content}>{props.children}</div>
-  
-      <Footer/>
+      <section
+        className={`${layoutStyles.layout} ${
+          props.page === "info" &&
+          layoutStyles.info_page}`}
+        style={{
+          backgroundColor: props.bgColor,
+        }}
+      >
+        
+        <Helmet>
+          <html lang="en" />
+          <title>{metaTitle}</title>
+          <meta name="description" content={metaDescription} />
+        </Helmet>
+        <HeaderSection title={title} />
+        
+        <div className={layoutStyles.content}>{props.children}</div>
+    
+        <Footer/>
     </section>
   )
 }
