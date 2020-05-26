ca22bbfec4bd68fc12cc4752cb5a82163c094df7
sergio.gordo <sergio@meta-site.com>
2020-05-26 10:42:55 +0200

    draft section

--- src/templates/blogMd.js
+++ src/templates/blogMd.js
@@ -5,6 +5,33 @@ import useBlogData from '../static_queries/useMdBlogData'
 import blogTemplateStyles from "../styles/templates/blog.module.scss"
 //this component handles the blur img & fade-ins
 import Img from 'gatsby-image'
+import Helmet from "react-helmet"
+
+function buildMetaTitle(title) {
+  if (!!title) {
+    return <title>{props.title}</title>
+  } else {
+    return null;
+  }
+}
+
+function buildMetaDescription(description) {
+  if (!!description) {
+    return <meta name="description" content={props.description}/>
+  } else {
+    return null;
+  }
+}
+
+function buildMetas(metas) {
+  if (!!metas) {
+    return <Helmet>
+      {buildMetaTitle(metas.title)}
+      {buildMetaDescription(metas.description)}
+    </Helmet>
+  }
+}
+
 
 export default function BlogMd(props) {
   const data = props.data.markdownRemark
@@ -26,6 +53,7 @@ export default function BlogMd(props) {
 
   return (
     <Layout>
+      {buildMetas(data.frontmatter.meta_data)}
       <article className={blogTemplateStyles.blog}>
         <figure className={blogTemplateStyles.blog__hero}>
           <Img
