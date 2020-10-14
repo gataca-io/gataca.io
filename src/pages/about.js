1fbce8492bc26c235536e98ff9c953e113829bbd
sergio gordo <sergormo@gmail.com>
2020-10-14 13:53:00 +0200

    fix images and tab centered

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../layouts/Layout"
 import aboutStyles from "../styles/pages/about.module.scss"
 // import useSiteMetaData from "../static_queries/useSiteMetadata"
-import aboutData  from "./../../content/data/aboutData.json"
+import aboutData from "./../../content/data/aboutData.json"
 import TeamSection from "../components/TeamSection/TeamSection"
 import { BuildHelmet } from "../components/auxiliary/HelmetBuilder"
 
@@ -17,7 +17,7 @@ function IntroSection({ data }) {
   )
 }
 
-function AwardsSection({data}) {
+function AwardsSection({ data }) {
   return (
     <section className={aboutStyles.awardsContainer}>
       <div>
@@ -25,19 +25,18 @@ function AwardsSection({data}) {
         <div className={aboutStyles.awards}>
           {data.awards.map(award => {
             return (
-                <a href={award.link} target="_blank" rel="noreferrer">
-              <div className={aboutStyles.award}>
-                
-                <div className={aboutStyles.photoContainer}>
-                  <img src={award.photo} alt={award.title}/>
+              <a href={award.link} target="_blank" rel="noreferrer">
+                <div className={aboutStyles.award}>
+                  <div className={aboutStyles.photoContainer}>
+                    <img src={award.photo} alt={award.title}/>
+                  </div>
+                  <h3>
+                    {award.title}
+                  </h3>
+                  <div dangerouslySetInnerHTML={{ __html: award.description }}>
+                  </div>
                 </div>
-                <h3>
-                  {award.title}
-                </h3>
-                <div dangerouslySetInnerHTML={{__html:award.description}}>
-                </div>
-              </div>
-                </a>
+              </a>
             )
           })}
         </div>
@@ -49,17 +48,17 @@ function AwardsSection({data}) {
 export default function About() {
   // const { infoData } = useSiteMetaData()
   return (
-      <Layout>
-        <BuildHelmet
-          title={aboutData.metas.title}
-          description={aboutData.metas.description}
-        />
-        <section className={aboutStyles.base}>
-          <IntroSection data={aboutData.introSection}/>
-          {/*<TeamSection data={aboutData.teamDataSection}/>*/}
-          <TeamSection/>
-          <AwardsSection data={aboutData.awardsDataSection}/>
-        </section>
+    <Layout>
+      <BuildHelmet
+        title={aboutData.metas.title}
+        description={aboutData.metas.description}
+      />
+      <section className={aboutStyles.base}>
+        <IntroSection data={aboutData.introSection}/>
+        {/*<TeamSection data={aboutData.teamDataSection}/>*/}
+        <TeamSection/>
+        <AwardsSection data={aboutData.awardsDataSection}/>
+      </section>
     </Layout>
   )
 }
