20e654d82b30a96e98fe896d21a34935ca69220b
meta-sergio <sergio@gataca.io>
2021-09-01 18:16:44 +0200

    test_cookies-removals

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import HeaderSection from "../components/HeaderSection/HeaderSection"
 import layoutStyles from "./layout.module.scss"
 import FooterSection from "../components/FooterSection/FooterSection"
@@ -9,35 +9,98 @@ import CookieConsent from "react-cookie-consent"
 export default function Layout(props) {
   // const { title } = useSiteMetadata()
   
-  const removeCookies = () => {
-    const _hsp = window._hsp = window._hsp || [];
-    document.getElementById("removeCookies").onclick = function() {
-      _hsp.push(['revokeCookieConsent']);
-    };
-    document.cookie = '_ga' + '=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = '_gid' + '=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    document.cookie = 'gataca_io_accepted' + '=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  
+  const removeAnalyticsCookies = () => {
+    console.log('removeAnalitics');
+    document.cookie = "_ga" + "=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+    document.cookie = "_gid" + "=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+    document.cookie = "gataca_io_accepted" + "=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+    document.cookie = "_gat_UA-133561010-1" + "=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+    document.cookie = "fs_uid" + "=; Path=/; Domain=.example.com; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
   }
   
-  return (
-      <section
-        className={`${layoutStyles.layout} ${
-          props.page === "info" &&
-          layoutStyles.info_page}`}
-        style={{
-          backgroundColor: props.bgColor,
-        }}
-      >
-        
-        <HeaderSection title={configMetaData.title} link={configMetaData}/>
-        
-        <div className={layoutStyles.content}>
-          {props.children}
-        </div>
-        
-        <FooterSection />
+  const deleteAllCookies = () => {
+    var cookies = document.cookie.split(";")
+    
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i]
+      var eqPos = cookie.indexOf("=")
+      var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    }
+  }
   
-{/*        <CookieConsent
+  const getCookie = (name) => {
+    var dc = document.cookie
+    var prefix = name + "="
+    var begin = dc.indexOf("; " + prefix)
+    if (begin == -1) {
+      begin = dc.indexOf(prefix)
+      if (begin != 0) return null
+    } else {
+      begin += 2
+      var end = document.cookie.indexOf(";", begin)
+      if (end == -1) {
+        end = dc.length
+      }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end))
+  }
+  
+  
+  useEffect(() => {
+    const hubspotStateCookie = getCookie("__hs_cookie_cat_pref")
+    if (hubspotStateCookie) {
+      const cookieStructure = hubspotStateCookie.split(",")
+      console.log(cookieStructure);
+      // Analytics
+      if (cookieStructure[0].includes("false")) {
+        removeAnalyticsCookies()
+      // Functionality
+      } else if (cookieStructure[1].includes("false")) {
+      
+      // Advertisementent
+      } else if (cookieStructure[0].includes("false")) {
+      
+      }
+    }
+    
+    const _hsp = window._hsp = window._hsp || []
+    const hubspotBanner = document.getElementById("hs-eu-decline-button")
+    if (hubspotBanner) {
+      hubspotBanner.onclick = function() {
+        deleteAllCookies()
+        _hsp.push(["revokeCookieConsent"])
+      }
+    }
+    
+    return () => {
+    
+    }
+  }, [])
+  
+  
+  return (
+    <section
+      className={`${layoutStyles.layout} ${
+        props.page === "info" &&
+        layoutStyles.info_page}`}
+      style={{
+        backgroundColor: props.bgColor,
+      }}
+    >
+      
+      <HeaderSection title={configMetaData.title} link={configMetaData}/>
+      
+      <div className={layoutStyles.content}>
+        {props.children}
+      </div>
+      
+      <FooterSection/>
+      
+      {/*        <CookieConsent
           location="bottom"
           buttonText="Accept"
           declineButtonText={"Decline"}
@@ -65,8 +128,8 @@ export default function Layout(props) {
           {configMetaData.cookies}{" "}
         </CookieConsent>
         */}
-        
-        
+    
+    
     </section>
   )
 }
