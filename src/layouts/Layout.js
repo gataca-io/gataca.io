e4c146d2dc52581e4b847dfb562bdd816920e92f
meta-sergio <sergio@gataca.io>
2022-01-19 13:16:32 +0100

    Remove mixpanel & stabilise

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -4,7 +4,6 @@ import layoutStyles from "./layout.module.scss"
 import FooterSection from "../components/FooterSection/FooterSection"
 import configMetaData from "../../content/data/configMetaData.json"
 
-import mixpanel from 'mixpanel-browser';
 export default function Layout(props) {
   
   const removeAnalyticsCookies = () => {
@@ -36,8 +35,6 @@ export default function Layout(props) {
   }
   
   useEffect(() => {
-    mixpanel.init('358bba736311028757bfcda6b89b3983', {debug: true});
-    mixpanel.track('Sign up');
     const hubspotStateCookie = getCookie("__hs_cookie_cat_pref")
     // console.log("HUBSPOT COOKIE => ", hubspotStateCookie)
     if (hubspotStateCookie) {
