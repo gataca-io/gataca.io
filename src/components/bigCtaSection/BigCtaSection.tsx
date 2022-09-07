803af583c939acc61ec06411b28a8c7a2974310e
angiebenavente <angela@gataca.io>
2022-09-07 17:07:18 +0200

    feature(launch and pre launch modals)

--- src/components/bigCtaSection/BigCtaSection.tsx
+++ src/components/bigCtaSection/BigCtaSection.tsx
@@ -1,33 +1,28 @@
-import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import React from "react"
 import homeData from "../../../content/data/homeData.json"
 import styles from "./bigcta.module.scss"
-import { Link } from "gatsby"
 
 type IBigCtaSectionProps = {}
 
 const BigCtaSection: React.FC<IBigCtaSectionProps> = ({}) => {
-
-
   return (
     <React.Fragment>
       <section className={styles.mainSection}>
-
         <div className={styles.subContainer}>
-
           <div className={styles.hero_container_mobile}>
-            <div className={styles.circle2}/>
-            <div className={styles.circle3}/>
-            <div className={styles.circle4}/>
-            <div className={styles.circle5}/>
+            <div className={styles.circle2} />
+            <div className={styles.circle3} />
+            <div className={styles.circle4} />
+            <div className={styles.circle5} />
             <div className={styles.gridContainer_mobile}>
               {homeData.newIntroSection.images.map((img, i) => {
-                return <div
-                  key={i}
-                  className={styles.imageContainerMobile}>
-                  <img src={img} alt={""}/>
-                </div>
-              })
-              }
+                return (
+                  <div key={i} className={styles.imageContainerMobile}>
+                    <img src={img} alt={""} />
+                  </div>
+                )
+              })}
             </div>
             {/*<img src={homeData.newIntroSection.image}/>*/}
           </div>
@@ -37,30 +32,31 @@ const BigCtaSection: React.FC<IBigCtaSectionProps> = ({}) => {
             <h3>{homeData.bigCtaSection.description}</h3>
             {/*<p>{homeData.bigCtaSection.subDescription}</p>*/}
             <Link to={homeData.bigCtaSection.ctaUrl}>
-              <button
-                className={styles.ctaButton}
-              > {homeData.bigCtaSection.subDescription}</button>
+              <button className={styles.ctaButton}>
+                {" "}
+                {homeData.bigCtaSection.subDescription}
+              </button>
             </Link>
           </div>
 
           <div className={styles.hero_container}>
-            <div className={styles.circleTop}/>
-            <div className={styles.circle2}/>
-            <div className={styles.circle3}/>
-            <div className={styles.circle4}/>
-            <div className={styles.circle5}/>
+            <div className={styles.circleTop} />
+            <div className={styles.circle2} />
+            <div className={styles.circle3} />
+            <div className={styles.circle4} />
+            <div className={styles.circle5} />
             <div className={styles.gridContainer}>
               {homeData.newIntroSection.images.map((img, i) => {
-                return <div key={i} className={styles.imageContainer}>
-                  <img src={img} alt={""}/>
-                </div>
-              })
-              }
+                return (
+                  <div key={i} className={styles.imageContainer}>
+                    <img src={img} alt={""} />
+                  </div>
+                )
+              })}
             </div>
             {/*<img src={homeData.newIntroSection.image}/>*/}
           </div>
         </div>
-
       </section>
     </React.Fragment>
   )
