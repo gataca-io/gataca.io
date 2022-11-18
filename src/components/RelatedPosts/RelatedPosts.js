e5cf834b60966e13c355714981a0b2df9f4f43b1
angiebenavente <angela@gataca.io>
2022-11-18 11:11:45 +0100

    feature(home): add new design

--- src/components/RelatedPosts/RelatedPosts.js
+++ src/components/RelatedPosts/RelatedPosts.js
@@ -1,95 +1,103 @@
-import { graphql, Link, PageProps, StaticQuery, useStaticQuery } from "gatsby"
-import React, { ReactElement } from "react"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import React from "react"
 import { SizeMe } from "react-sizeme"
 import StackGrid from "react-stack-grid"
 import blogListStyles from "../BlogList/bloglist.module.scss"
 import Img from "gatsby-image"
+import homeData from "./../../../content/data/homeData.json"
 import styles from "./relatedposts.module.scss"
 
 function BuildImage(image) {
   const imageEl = image.hero_image
-  return !!imageEl ? <Img
-    className={`${blogListStyles.imageContainer}`}
-    fluid={
-      imageEl.childImageSharp.fluid
-    }
-    alt={image.title}
-  /> : null
+  return !!imageEl ? (
+    <Img
+      className={`${blogListStyles.imageContainer}`}
+      fluid={imageEl.childImageSharp.fluid}
+      alt={image.title}
+    />
+  ) : null
 }
 
 const RelatedPosts = ({}) => {
-  
   const data = useStaticQuery(graphql`
-      query getLastBlogs {
-          allMarkdownRemark(
-              sort: { order: DESC, fields: frontmatter___date },
-              limit: 3,
-              filter: {fileAbsolutePath: {regex: "/posts/.*\\\\.md$/"}}
-          ) {
-              edges {
-                  node {
-                      id
-                      frontmatter {
-                          date(formatString: "MMMM Do, YYYY")
-                          title
-                          hero_image {
-                              childImageSharp {
-                                  fluid( maxWidth: 800 ) {
-                                      ...GatsbyImageSharpFluid
-                                  }
-                              }
-                          }
-                      }
-                      excerpt(pruneLength: 200)
-                      fields {
-                          slug
-                      }
+    query getLastBlogs {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: frontmatter___date }
+        limit: 3
+        filter: { fileAbsolutePath: { regex: "/posts/.*\\\\.md$/" } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              date(formatString: "MMMM Do, YYYY")
+              title
+              hero_image {
+                childImageSharp {
+                  fluid(maxWidth: 800) {
+                    ...GatsbyImageSharpFluid
                   }
+                }
               }
+            }
+            excerpt(pruneLength: 190)
+            fields {
+              slug
+            }
           }
+        }
       }
+    }
   `)
-  
+
   const posts = data.allMarkdownRemark.edges
-  
+
   return (
     <section className={styles.mainContainer}>
       <div className={styles.headerContainer}>
-        <h3>Latest insights</h3>
+        <h3>{homeData.sixthSection.title}</h3>
       </div>
       <SizeMe>
         {({ size }) => {
-          return <StackGrid
-            appearDelay={20}
-            gutterWidth={20}
-            gutterHeight={20}
-            columnWidth={
-              (size.width >= 672 && size.width <= 1100) ? "33%"
-                : size.width <= 672 ? "100%"
-                  : "33%"
-            }
-          >
-            {posts
-              .filter((blog) => blog.node.frontmatter.title !== "")
-              .map((blog) => {
-                return (
-                  <div className={styles.post} key={blog.node.id}>
-                    <Link to={`/blog/${blog.node.fields.slug}`}>
-                      <li key={blog.node.fields.slug}>
-                        <div className={styles.list__hero}>
-                          {BuildImage(blog.node.frontmatter)}
-                        </div>
-                        <div className={styles.list__info}>
-                          <h3>{blog.node.frontmatter.title}</h3>
-                          <p>{blog.node.excerpt}</p>
-                          <p className={styles.date}>{blog.node.frontmatter.date}</p>
-                        </div>
-                      </li>
-                    </Link>
-                  </div>
-                )
-              })}
-          </StackGrid>
+          return (
+            <StackGrid
+              appearDelay={20}
+              gutterWidth={20}
+              gutterHeight={size.width >= 672 ? 10 : 20}
+              columnWidth={
+                size.width >= 672 && size.width <= 1100
+                  ? "27%"
+                  : size.width <= 672
+                  ? "100%"
+                  : "27%"
+              }
+            >
+              {posts
+                .filter((blog) => blog.node.frontmatter.title !== "")
+                .map((blog) => {
+                  return (
+                    <div className={styles.post} key={blog.node.id}>
+                      <Link to={`/blog/${blog.node.fields.slug}`}>
+                        <li key={blog.node.fields.slug}>
+                          <div className={styles.list__hero}>
+                            {BuildImage(blog.node.frontmatter)}
+                          </div>
+                          <div className={styles.list__info}>
+                            <h3>{blog.node.frontmatter.title}</h3>
+                            <p className={styles.date}>
+                              {blog.node.frontmatter.date}
+                            </p>
+                            <p className={styles.excerpt}>
+                              {blog.node.excerpt}
+                            </p>
+                          </div>
+                        </li>
+                      </Link>
+                    </div>
+                  )
+                })}
+            </StackGrid>
+          )
         }}
       </SizeMe>
     </section>
@@ -97,5 +105,3 @@ const RelatedPosts = ({}) => {
 }
 
 export default RelatedPosts
-
-
--- src/components/bigCtaSection/BigCtaSection.tsx
+++ src/components/bigCtaSection/BigCtaSection.tsx
@@ -1,66 +1,33 @@
-import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
+import React from "react"
 import homeData from "../../../content/data/homeData.json"
 import styles from "./bigcta.module.scss"
-import { Link } from "gatsby"
 
 type IBigCtaSectionProps = {}
 
 const BigCtaSection: React.FC<IBigCtaSectionProps> = ({}) => {
-
-
   return (
     <React.Fragment>
       <section className={styles.mainSection}>
-
         <div className={styles.subContainer}>
-
-          <div className={styles.hero_container_mobile}>
-            <div className={styles.circle2}/>
-            <div className={styles.circle3}/>
-            <div className={styles.circle4}/>
-            <div className={styles.circle5}/>
-            <div className={styles.gridContainer_mobile}>
-              {homeData.newIntroSection.images.map((img, i) => {
-                return <div
-                  key={i}
-                  className={styles.imageContainerMobile}>
-                  <img src={img} alt={""}/>
-                </div>
-              })
-              }
-            </div>
-            {/*<img src={homeData.newIntroSection.image}/>*/}
-          </div>
-
           <div className={styles.textBox}>
             <h1>{homeData.bigCtaSection.title}</h1>
             <h3>{homeData.bigCtaSection.description}</h3>
-            {/*<p>{homeData.bigCtaSection.subDescription}</p>*/}
             <Link to={homeData.bigCtaSection.ctaUrl}>
-              <button
-                className={styles.ctaButton}
-              > {homeData.bigCtaSection.subDescription}</button>
+              <button className={styles.ctaButton}>
+                {" "}
+                {homeData.bigCtaSection.ctaText}
+              </button>
             </Link>
           </div>
 
           <div className={styles.hero_container}>
-            <div className={styles.circleTop}/>
-            <div className={styles.circle2}/>
-            <div className={styles.circle3}/>
-            <div className={styles.circle4}/>
-            <div className={styles.circle5}/>
-            <div className={styles.gridContainer}>
-              {homeData.newIntroSection.images.map((img, i) => {
-                return <div key={i} className={styles.imageContainer}>
-                  <img src={img} alt={""}/>
-                </div>
-              })
-              }
-            </div>
-            {/*<img src={homeData.newIntroSection.image}/>*/}
+            <img
+              src={homeData.bigCtaSection.image}
+              alt={homeData.bigCtaSection.title}
+            />
           </div>
         </div>
-
       </section>
     </React.Fragment>
   )
