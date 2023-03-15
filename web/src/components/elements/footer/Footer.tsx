069441c436d6feecb1aa1c2b013cf78cc84f077f
Mayra <mayra@gataca.io>
2023-03-15 12:59:01 +0100

    feature(footer): delete container tags

--- web/src/components/elements/footer/Footer.tsx
+++ web/src/components/elements/footer/Footer.tsx
@@ -16,51 +16,47 @@ import SectionContent from "./footerComponents/SectionContent"
 
 const Footer: React.FC = () => {
   return (
-    <main>
-      <>
-        <footer className={styles?.footer}>
-          <section className={styles?.mainSectionFooter}>
-            <div className="logo">
-              <img src={images.gatacIconAndText} />
-            </div>
-            <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
-              <SectionContent navigationObject={productsFooterMenu} />
-            </div>
-            <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
-              <SectionContent navigationObject={useCasesFooterMenu} />
-              <SectionContent navigationObject={useCasesFooterMenu} />
-            </div>
-            <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
-              <SectionContent navigationObject={companyFooterMenu} />
-              <SectionContent navigationObject={learnFooterMenu} />
-            </div>
-          </section>
-          <hr></hr>
-          <section className={styles?.secondarySectionFooter}>
-            <div className={cx("marginBottom16")}>
-              <ul>
-                {subFooterMenu?.subRoutes?.map(item => {
-                  return (
-                    <li>
-                      <Link className={cx("buttonMD")} to={item.route || ""}>
-                        {item.label}
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </div>
-            <div
-              className={`${styles?.rightsReserved} ${cx("buttonMD")} ${cx(
-                "marginBottom16"
-              )}`}
-            >
-              {rightsReservedGataca?.label}
-            </div>
-          </section>
-        </footer>
-      </>
-    </main>
+    <footer className={styles?.footer}>
+      <section className={styles?.mainSectionFooter}>
+        <div className="logo">
+          <img src={images.gatacIconAndText} />
+        </div>
+        <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
+          <SectionContent navigationObject={productsFooterMenu} />
+        </div>
+        <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
+          <SectionContent navigationObject={useCasesFooterMenu} />
+          <SectionContent navigationObject={useCasesFooterMenu} />
+        </div>
+        <div className={`${styles?.footerSections} ${cx("marginTop10")}`}>
+          <SectionContent navigationObject={companyFooterMenu} />
+          <SectionContent navigationObject={learnFooterMenu} />
+        </div>
+      </section>
+      <hr></hr>
+      <section className={styles?.secondarySectionFooter}>
+        <div className={cx("marginBottom16")}>
+          <ul>
+            {subFooterMenu?.subRoutes?.map(item => {
+              return (
+                <li>
+                  <Link className={cx("buttonMD")} to={item.route || ""}>
+                    {item.label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div
+          className={`${styles?.rightsReserved} ${cx("buttonMD")} ${cx(
+            "marginBottom16"
+          )}`}
+        >
+          {rightsReservedGataca?.label}
+        </div>
+      </section>
+    </footer>
   )
 }
 
