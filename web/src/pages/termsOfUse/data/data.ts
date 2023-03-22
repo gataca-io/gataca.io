ff8da808d300d5c730a37624f305e68061163694
Mayra <mayra@gataca.io>
2023-03-22 16:13:41 +0100

    feature(term of use): new view + data

---
+++ web/src/pages/termsOfUse/data/data.ts
@@ -0,0 +1,89 @@
+import { LinkModel } from "../../../interfaces/interfaces"
+
+// URLs/URIs
+export const legalGatacaEmail = "legal@gataca.io"
+export const gatacaURL = "https://gataca.io"
+export const privacyPolicyURL = "/privacyPolicy"
+export const cookiePolicyURL = "/cookiePolicy"
+
+export const tableOfContent: LinkModel[] = [
+  {
+    id: "termsOfUse",
+    label: "Table of content",
+    subRoutes: [
+      {
+        id: "introduction",
+        label: "1. Introduction",
+        route: "#introduction",
+      },
+      {
+        id: "acceptanceOfTerms",
+        label: "2. Acceptance of Terms",
+        route: "#acceptanceOfTerms",
+      },
+      {
+        id: "rightsUseOfService",
+        label: "3. Your rights to use the Services",
+        route: "#rightsUseOfService",
+      },
+      {
+        id: "definition",
+        label: "4. Definition",
+        route: "#definition",
+      },
+      {
+        id: "responsibleUse",
+        label: "5. Responsible Use",
+        route: "#responsibleUse",
+      },
+      {
+        id: "intellectualPropertyRights",
+        label: "6. Intellectual property rights",
+        route: "#intellectualPropertyRights",
+      },
+      {
+        id: "warrantiesLiabilitiesIndemnity",
+        label: "7. Warranties, liabilities, and indemnity",
+        route: "#warrantiesLiabilitiesIndemnity",
+      },
+      {
+        id: "dataProtection",
+        label: "8. Data protection",
+        route: "#dataProtection",
+      },
+      {
+        id: "linksOtherSites",
+        label: "9. Links to other sites",
+        route: "#linksOtherSites",
+      },
+      {
+        id: "communications",
+        label: "10. Communications",
+        route: "#communications",
+      },
+      {
+        id: "amendmentTermination",
+        label: "11. Amendment and termination",
+        route: "#amendmentTermination",
+      },
+      {
+        id: "agreementEntirety",
+        label: "12. Agreement in its entirety",
+        route: "#agreementEntirety",
+      },
+      {
+        id: "lawJurisdiction",
+        label: "13. Applicable law and jurisdiction",
+        route: "#lawJurisdiction",
+      },
+      {
+        id: "contact",
+        label: "14. Contact",
+        route: "#contact",
+      },
+    ],
+  },
+]
+export const termsOfUseOptions = tableOfContent?.find(
+  el => el?.id === "termsOfUse"
+)
---
+++ web/src/pages/termsOfUse/index.tsx
@@ -0,0 +1,142 @@
+import { Link, PageProps } from "gatsby"
+import * as React from "react"
+import * as styles from "./index.module.scss"
+import cx from "classnames"
+import Layout from "../../components/templates/mainLayout/MainLayout"
+import TableOfContent from "../../components/templates/tableOfContent/TableOfContent"
+import {
+  termsOfUseOptions,
+  gatacaURL,
+  privacyPolicyURL,
+  cookiePolicyURL,
+} from "./data/data"
+
+const TermsOfUse: React.FC<PageProps> = () => {
+  return (
+    <Layout>
+      <section className={`${styles?.termsOfUse} ${cx("containerMaxWidth")}`}>
+        <TableOfContent
+          navigationObject={termsOfUseOptions}
+          className={styles?.showMobile}
+        />
+
+        <div className={styles?.sectionHeader}>
+          <h1 className={cx("heading1 marginBottom32")}>Terms of use</h1>
+          <p className={cx("bodyRegularXL")}>Effective date: August 31, 2022</p>
+        </div>
+        <div className={styles?.sectionMain}>
+          <div className={styles?.sectionMain__dataContentCol}>
+            <div className={styles?.contentCol__section} id="introduction">
+              <h4 className={cx("heading4 marginBottom32")}>1. Introduction</h4>
+              <p className={cx("marginBottom20")}>
+                Gataca España S.L.U and its affiliates (referred to as “
+                <b>Gataca</b>” or “<b>we</b>”, “<b>our</b>” or “<b>us</b>”) is a
+                cybersecurity company that provides decentralized digital
+                identity technology through our software applications
+                (collectively, “<b>Gataca Platform</b>”), including but not
+                limited to our website
+                <Link to={gatacaURL}> https://gataca.io</Link> (“<b>Website</b>
+                ”), a cloud-based end-to-end solution (“<b>Gataca Studio</b>”),
+                our on-premise applications GATACA Certify and GATACA Connect
+                and a mobile application (“
+                <b>Gataca Wallet</b>”).
+              </p>
+              <p className={cx("marginBottom20")}>
+                “<b>Services</b>” herein refer to any and all website,
+                application (web, mobile, desktop, cloud-based or otherwise),
+                APIs, or other services provided by GATACA.
+              </p>
+              <p className={cx("marginBottom20")}>
+                The Services includes code, images, text, and other materials,
+                and third-party content (collectively the “<b>Contents</b>”).
+              </p>
+              <p className={cx("marginBottom20")}>
+                The terms “<b>you</b>”, “<b>your</b>”, “and “<b>Client</b>”
+                refer to you, whether you are an individual or a legal entity.
+                These Terms of Service (“<b>Terms</b>”) is a legal agreement
+                between you and GATACA.
+              </p>
+            </div>
+            <div className={styles?.contentCol__section} id="acceptanceOfTerms">
+              <h4 className={cx("heading4 marginBottom32")}>
+                2. Acceptance of Terms
+              </h4>
+              <p className={cx("marginBottom20")}>
+                To use the Services, you must review and accept these Terms. By
+                clicking “I Agree,” indicating acceptance electronically, or by
+                installing, accessing or using our Services, you agree to a
+                full, complete and unreserved acceptance of each and every one
+                of the terms included in these Terms. Unless otherwise stated in
+                the corresponding Service-specific terms, you must be over 14 to
+                be able to access or use the Services.
+              </p>
+              <p className={cx("marginBottom20")}>
+                These Terms include by reference:
+              </p>
+              <ul>
+                <li className={cx("marginBottom20")}>
+                  <p>
+                    GATACA’s <Link to={privacyPolicyURL}>Privacy Policy</Link>
+                  </p>
+                </li>
+                <li className={cx("marginBottom20")}>
+                  <p>
+                    GATACA’s <Link to={cookiePolicyURL}>Cookies Policy</Link>
+                  </p>
+                </li>
+                <li className={cx("marginBottom20")}>
+                  <p>
+                    GATACA’s service-specific terms, including, but not limited
+                    to, GATACA Wallet Terms of Service and GATACA Studio Terms
+                    of Service, when applicable.
+                  </p>
+                </li>
+                <li className={cx("marginBottom20")}>
+                  <p>
+                    Any terms provided separately to you for specific Services,
+                    including product terms, payment terms, etc.
+                  </p>
+                </li>
+                <li className={cx("marginBottom20")}>
+                  <p>
+                    Additional terms and conditions, which may include those
+                    from third parties.
+                  </p>
+                </li>
+              </ul>
+              <p className={cx("marginBottom20")}>
+                <b>
+                  IF YOU DO NOT AGREE TO ALL THESE TERMS, YOU SHALL REFRAIN FROM
+                  USING THE SERVICES.
+                </b>
+              </p>
+            </div>
+            <div
+              className={styles?.contentCol__section}
+              id="rightsUseOfService"
+            >
+              <h4 className={cx("heading4 marginBottom32")}>
+                3. Your rights to use the Services
+              </h4>
+              <p className={cx("marginBottom20")}>
+                The Services are protected by copyright, trade secret, and other
+                intellectual property laws. You are only granted the right to
+                use the Services. No other right of use, alteration,
+                exploitation, reproduction, distribution or public communication
+                of the Services and/or its Contents other than those expressly
+                provided for herein is hereby conferred on you. GATACA reserves
+                all other rights in the Services.
+              </p>
+            </div>
+          </div>
+          <TableOfContent
+            navigationObject={termsOfUseOptions}
+            className={styles?.showDesktop}
+          />
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default TermsOfUse
