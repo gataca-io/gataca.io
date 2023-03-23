bacba1cfeb0ec86edd85880d1570ae7deaf066b7
Mayra <mayra@gataca.io>
2023-03-23 09:51:06 +0100

    feature(dropdown): add categories for use cases & sectors

--- web/src/components/elements/header/headerComponents/menuSubOption/menuSubOption.module.scss.d.ts
+++ web/src/components/elements/header/headerComponents/menuSubOption/menuSubOption.module.scss.d.ts
@@ -1,2 +1,5 @@
 export const menuSubOption: string
 export const menuSubOptionItem: string
+export const menuCategory: string
+export const menuCategoryItem: string
+export const menuCategoryItem__title: string
