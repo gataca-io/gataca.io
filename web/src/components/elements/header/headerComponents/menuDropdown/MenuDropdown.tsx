d9957a1a3c7c466306c88d5e5a15572900c0df4d
Mayra <mayra@gataca.io>
2023-03-24 14:26:17 +0100

    feature(dropdown): icon svg to allow hover color change

--- web/src/components/elements/header/headerComponents/menuDropdown/MenuDropdown.tsx
+++ web/src/components/elements/header/headerComponents/menuDropdown/MenuDropdown.tsx
@@ -22,7 +22,22 @@ const MenuDropdown: React.FC<IMenuDropdownProps> = props => {
     >
       <div className={`${styles.dropdownLabel} ${cx("buttonMD")}`}>
         {item?.label}
-      <img src={images.chevronDown} />
+        <svg
+          className={styles.chevronDown}
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M4 6L8 10L12 6"
+            stroke="currentColor"
+            stroke-width="1.33333"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </div>
 
       <MenuSubOption item={item} open={open} />
