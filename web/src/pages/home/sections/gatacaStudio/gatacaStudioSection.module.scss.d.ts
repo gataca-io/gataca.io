7e44754bcde2215b0aefe6607dfaa90ec389445b
Mayra <mayra@gataca.io>
2023-04-05 16:16:20 +0200

    feature(thirdSection): rename

--- web/src/pages/home/sections/gatacaStudio/gatacaStudioSection.module.scss.d.ts
+++
@@ -1,7 +0,0 @@
-export const showDesktop: string
-export const showMobile: string
-export const description: string
-export const gatacaStudioSection: string
-export const gatacaStudioSection__container: string
-export const gatacaStudioSection__leftColumn: string
-export const gatacaStudioSection__rightColumn: string
