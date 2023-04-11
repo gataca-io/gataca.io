3a792a045d16da6ad1e6d9b15ead1d0312bf3ee4
Mayra <mayra@gataca.io>
2023-04-11 11:53:56 +0200

    feature(firstSection): new section

--- web/src/pages/useCasesSectors/government/index.tsx
+++ web/src/pages/useCasesSectors/government/index.tsx
@@ -3,8 +3,19 @@ import * as React from "react"
 import Layout from "../../../components/templates/mainLayout/MainLayout"
 import PreFooterCTASection from "../../../components/templates/sections/preFooterCTA/PreFooterCTA"
 import { navigate } from "gatsby"
+import { useState } from "react"
 
 const GovernmentPage: React.FC<PageProps> = () => {
+  const [governmentData, setGovernment] = useState<any | undefined>()
+  const {} = governmentData ? governmentData : []
+
+  React.useEffect(() => {
+    getGovernment()
+  }, [])
+  const getGovernment = async () => {
+    const json_data = require("./data/governmentData.json")
+    setGovernment(json_data?.data && json_data?.data)
+  }
   return (
     <Layout>
       <>
