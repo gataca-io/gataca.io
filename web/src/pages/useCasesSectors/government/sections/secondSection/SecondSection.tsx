a67d3f6f6ca0215236a48ff536e1c3acae608499
Mayra <mayra@gataca.io>
2023-04-12 11:58:24 +0200

    feature(secondSection): styles

--- web/src/pages/useCasesSectors/government/sections/secondSection/SecondSection.tsx
+++ web/src/pages/useCasesSectors/government/sections/secondSection/SecondSection.tsx
@@ -15,18 +15,25 @@ const SecondSection: React.FC<ISectionProps> = props => {
 
   return (
     <section className={`${styles?.stepsSection} ${cx("containerMaxWidth")}`}>
-      <div className={styles?.stepsSection__container}>
-        {steps?.map((item, index) => {
-          const { title, description, number } = item
-          return (
-            <div className={styles?.stepsSection__container}>
+      {steps?.map((item, index) => {
+        const { title, description, number } = item
+        return (
+          <div
+            key={"stepsSection__container__" + index}
+            className={styles?.stepsSection__container}
+          >
+            <div className={styles?.numberContainer}>
               <div className={styles?.number}>{number}</div>
-              <h3 className={cx("marginBottom12 heading3")}>{title}</h3>
-              <p className={cx("bodyRegularXL neutral700")}>{description}</p>
             </div>
-          )
-        })}
-      </div>
+            <div className={styles?.textContainer}>
+              <div className={styles?.text}>
+                <h3 className={cx("marginBottom12 heading3")}>{title}</h3>
+                <p className={cx("bodyRegularXL neutral700")}>{description}</p>
+              </div>
+            </div>
+          </div>
+        )
+      })}
     </section>
   )
 }
