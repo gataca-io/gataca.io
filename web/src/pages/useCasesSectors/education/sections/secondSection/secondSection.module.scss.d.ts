03a11c07ffea9e2137038ef7f54606596f37797c
Mayra <mayra@gataca.io>
2023-04-13 09:43:28 +0200

    feature(education): new page

---
+++ web/src/pages/useCasesSectors/education/sections/secondSection/secondSection.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const stepsSection: string
+export const stepsSection__container: string
+export const numberContainer: string
+export const number: string
+export const textContainer: string
+export const text: string
