6a9b558813fee3a29fa7890e930d41fba92fa4ee
angiebenavente <angela@gataca.io>
2023-04-20 11:40:22 +0200

    feature(environments): add development and default

--- web/src/components/atoms/images/StrapiImage.tsx
+++ web/src/components/atoms/images/StrapiImage.tsx
@@ -10,11 +10,11 @@ const StrapiImage: React.FC<IStrapiImageProps> = props => {
   const { image, className } = props
   const imageAtt = image?.data?.attributes
   const imageURL = image?.data?.attributes?.url
-  // TODO : Change "http://127.0.0.1:1337" by env variable for stripe API when Admin is deployed
+
   return imageURL ? (
     <img
       className={className}
-      src={"http://127.0.0.1:1337" + imageAtt?.url}
+      src={process.env.STRAPI_API_URL + imageAtt?.url}
       alt={imageAtt?.alternativeText}
     />
   ) : null
