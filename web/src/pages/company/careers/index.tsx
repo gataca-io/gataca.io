9a79e57e723dfc83da3b600e8d6b5015ef573f9c
Mayra <mayra@gataca.io>
2023-04-21 13:58:31 +0200

    feature(secondSection): data and styles

--- web/src/pages/company/careers/index.tsx
+++ web/src/pages/company/careers/index.tsx
@@ -3,25 +3,33 @@ import * as React from "react"
 import { useState } from "react"
 import Layout from "../../../components/templates/mainLayout/MainLayout"
 import FirstSection from "./sections/firstSection/FirstSection"
+import SecondSection from "./sections/secondSection/SecondSection"
 
 const CareersPage: React.FC<PageProps> = () => {
   const [careersData, setCareers] = useState<any | undefined>()
-  const { firstSection } = careersData ? careersData : []
+  const { firstSection, secondSection } = careersData ? careersData : []
 
   React.useEffect(() => {
-    getaboutUs()
+    getCareers()
   }, [])
-  const getaboutUs = async () => {
+  const getCareers = async () => {
     const json_data = require("./data/careersData.json")
     setCareers(json_data?.data && json_data?.data)
   }
 
   return (
     <Layout>
-      <FirstSection
-        title={firstSection?.title}
-        descriptionParagraphs={firstSection?.descriptionParagraphs}
-      />
+      <>
+        <FirstSection
+          title={firstSection?.title}
+          descriptionParagraphs={firstSection?.descriptionParagraphs}
+        />
+        <SecondSection
+          title={secondSection?.title}
+          description={secondSection?.description}
+          list={secondSection?.list}
+        />
+      </>
     </Layout>
   )
 }
