687c73af3fa1cc672985d41627943c34e30ccfb7
Mayra <mayra@gataca.io>
2023-04-21 13:13:49 +0200

    feature(firstSection): data, styles and images

--- web/src/pages/company/careers/index.tsx
+++ web/src/pages/company/careers/index.tsx
@@ -1,13 +1,27 @@
 import type { PageProps } from "gatsby"
 import * as React from "react"
+import { useState } from "react"
 import Layout from "../../../components/templates/mainLayout/MainLayout"
+import FirstSection from "./sections/firstSection/FirstSection"
 
 const CareersPage: React.FC<PageProps> = () => {
+  const [careersData, setCareers] = useState<any | undefined>()
+  const { firstSection } = careersData ? careersData : []
+
+  React.useEffect(() => {
+    getaboutUs()
+  }, [])
+  const getaboutUs = async () => {
+    const json_data = require("./data/careersData.json")
+    setCareers(json_data?.data && json_data?.data)
+  }
+
   return (
     <Layout>
-      <main>
-        <h1>Careers</h1>
-      </main>
+      <FirstSection
+        title={firstSection?.title}
+        descriptionParagraphs={firstSection?.descriptionParagraphs}
+      />
     </Layout>
   )
 }
