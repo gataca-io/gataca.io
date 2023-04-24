46008d51c057d0bfdd83cc675e927311acd97f1e
angiebenavente <angela@gataca.io>
2023-04-24 16:21:30 +0200

    clean code

--- web/src/pages/pricing/components/licensesTableMobile/licensesTableMobile.module.scss.d.ts
+++ web/src/pages/pricing/components/licensesTableMobile/licensesTableMobile.module.scss.d.ts
@@ -1,25 +1,6 @@
 export const tableContainer: string
-export const table__header: string
-export const periodToggle: string
-export const periodCheckbox: string
-export const periodCheckboxLabel: string
 export const header__container: string
-export const table__header__cell: string
-export const table__header__cell__content: string
-export const licenseName: string
-export const license__header__popularTag: string
-export const licenseAdapted: string
 export const license__button: string
-export const table__body: string
-export const table__body__row: string
-export const card__feature: string
-export const table__body__row__category: string
-export const table__body__row__subcategory: string
-export const table__body__row__data: string
-export const card__feature__icon: string
-
-//
-
 export const card: string
 export const selector: string
 export const card__header: string
