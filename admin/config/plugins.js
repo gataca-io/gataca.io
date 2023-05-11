8439b7ee399fbd07944939773d1eece3a10df4b0
Mayra <mayra@gataca.io>
2023-05-11 16:58:54 +0200

    erase ckeditor pluggin

--- admin/config/plugins.js
+++ admin/config/plugins.js
@@ -2,10 +2,6 @@ module.exports = ({ env }) => ({
   seo: {
     enabled: true,
   },
-  ckeditor: {
-    enabled: true,
-    resolve: "./src/plugins/strapi-plugin-ckeditor",
-  },
   "strapi-plugin-populate-deep": {
     config: {
       defaultDepth: 3, // Default is 5
