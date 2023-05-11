f7075513cf7929eff7520308c838b381c84faf76
Samuel Gómez <samuel@gataca.io>
2023-05-11 17:13:48 +0200

    Modify Gataca URL and logos

--- web/src/data/defaultMetaData.ts
+++ web/src/data/defaultMetaData.ts
@@ -1,14 +1,14 @@
 import { marketAndroidURI, marketIosURI } from "./globalData"
 
 export const defaultMetadata = {
-  title: "GATACA",
+  title: "Gataca",
   cookies:
     "We use basic navigation cookies to get anonymous information about how users navigate this website in order to improve the user experience and understand the analytics behind it.",
   metaTitle: "Gataca | Decentralized Identity Management",
   description:
     "Gataca is a cybersecurity company that provides fast, compliant and secure digital identity technology solutions.",
   bookACallUrl: "/company/contact",
-  siteUrl: "https://www.gataca.io",
+  siteUrl: "https://gataca.io",
   twitterHandler: "@gataca_id",
   androidLink: marketAndroidURI,
   iosLink: marketIosURI,
