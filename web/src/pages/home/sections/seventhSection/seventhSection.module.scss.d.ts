0f7c4b2294cdebac11a470ba34cb1c76dbdd68b3
angiebenavente <angela@gataca.io>
2023-05-16 14:20:06 +0200

    feature(home): add clients feedback section

---
+++ web/src/pages/home/sections/seventhSection/seventhSection.module.scss.d.ts
@@ -0,0 +1,6 @@
+export const seventhSection: string
+export const seventhSection__container: string
+export const seventhSection__options: string
+export const seventhSection__leftSide: string
+export const seventhSection__rightSide: string
+export const seventhSection__leftSide__feedbackBullet: string
