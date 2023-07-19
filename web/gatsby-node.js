145dfb785168af434fd8985feb8149aa36ef8789
angiebenavente <102033882+angiebenavente@users.noreply.github.com>
2023-07-19 14:50:06 +0200

    remove unnecesary librrary (#221)
    * remove unnecesary librrary
    
    * remove unnecesary librrary

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -63,16 +63,3 @@ exports.createPages = ({ actions, graphql }) => {
   // Query for articles nodes to use in creating pages.
   return getArticles
 }
-
-const FilterWarningsPlugin = require("webpack-filter-warnings-plugin")
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    plugins: [
-      new FilterWarningsPlugin({
-        exclude:
-          /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
-      }),
-    ],
-  })
-}
