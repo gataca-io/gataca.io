2c37f02d60a7075d05a545b7588f79c84c7cc6ea
Mayra <mayra@gataca.io>
2023-10-26 13:09:55 +0200

    blog author info updated

--- web/src/components/atoms/strapi/StrapiAuthor.tsx
+++ web/src/components/atoms/strapi/StrapiAuthor.tsx
@@ -7,23 +7,118 @@ export interface IStrapiAuthorProps {
   className?: string
 }
 
+const teamImages = [
+  images.ceoTeam,
+  images.ctoTeam,
+  images.csoTeam,
+  images.businessTeam,
+  images.backendTeam,
+  images.frontend1Team,
+  images.designTeam,
+  images.itManagerTeam,
+  images.marketingTeam,
+  images.frontend2Team,
+]
+
 const StrapiAuthor: React.FC<IStrapiAuthorProps> = props => {
   const { author, className } = props
-  const authorAtt = author
+
+  let authorName = ""
+  let authorRole = ""
+  let authorImage = ""
+  let authorLinkedInURL = ""
+
+  const updateAuthorData = () => {
+    if (author === "Irene") {
+      return (
+        (authorName = "Irene Hernández"),
+        (authorRole = "CEO"),
+        (authorImage = teamImages[0]),
+        (authorLinkedInURL = "https://www.linkedin.com/in/samuelgoes/")
+      )
+    } else if (author === "Samuel") {
+      return (
+        (authorName = "Samuel Gómez"),
+        (authorRole = "CTO"),
+        (authorImage = teamImages[1]),
+        (authorLinkedInURL = "https://www.linkedin.com/in/samuelgoes/")
+      )
+    } else if (author === "José") {
+      return (
+        (authorName = "José San Juan"),
+        (authorRole = "CSO"),
+        (authorImage = teamImages[2]),
+        (authorLinkedInURL = "https://www.linkedin.com/in/jsanjup/")
+      )
+    } else if (author === "Chiara") {
+      return (
+        (authorName = "Chiara Casoni"),
+        (authorRole = "Business Development & Partnerships"),
+        (authorImage = teamImages[3]),
+        (authorLinkedInURL = "https://www.linkedin.com/in/chiaracasoni/")
+      )
+    } else if (author === "Daniel") {
+      return (
+        (authorName = "Daniel Moledo"),
+        (authorRole = "Backend Developer"),
+        (authorImage = teamImages[4]),
+        (authorLinkedInURL =
+          "https://www.linkedin.com/in/daniel-moledo-garcia/")
+      )
+    } else if (author === "Ángela") {
+      return (
+        (authorName = "Ángela Benavente"),
+        (authorRole = "Frontend Developer"),
+        (authorImage = teamImages[5]),
+        (authorLinkedInURL =
+          "https://www.linkedin.com/in/angela-benavente-almazan/")
+      )
+    } else if (author === "Tefy") {
+      return (
+        (authorName = "Estefanía Zambrano"),
+        (authorRole = "UX/UI Designer"),
+        (authorImage = teamImages[6]),
+        (authorLinkedInURL = "https://www.linkedin.com/in/tefy-zambrano/")
+      )
+    } else if (author === "Jorge") {
+      return (
+        (authorName = "Jorge Navas"),
+        (authorRole = "IT Manager"),
+        (authorImage = teamImages[7]),
+        (authorLinkedInURL =
+          "https://www.linkedin.com/in/jorge-navas-d%C3%ADaz-8b422643/")
+      )
+    } else if (author === "Esther") {
+      return (
+        (authorName = "Esther Saurí"),
+        (authorRole = "Digital Marketing Specialist"),
+        (authorImage = teamImages[8]),
+        (authorLinkedInURL =
+          "https://www.linkedin.com/in/esther-sauri-carbonell/")
+      )
+    } else if (author === "Mayra") {
+      return (
+        (authorName = "Mayra Amador"),
+        (authorRole = "Frontend Developer"),
+        (authorImage = teamImages[9]),
+        (authorLinkedInURL =
+          "https://www.linkedin.com/in/mayra-amador-zapata-/")
+      )
+    }
+  }
+
+  updateAuthorData()
 
   return (
     <div className={className}>
       <div>
-        <img src={images.marketingTeam}></img>
+        <img src={authorImage}></img>
       </div>
       <div>
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/esther-sauri-carbonell/"
-        >
-          <h6 className={cx("heading6")}>{authorAtt} Saurí</h6>
+        <a target="_blank" href={authorLinkedInURL}>
+          <h6 className={cx("heading6")}>{authorName}</h6>
         </a>
-        <p className={cx("bodyRegularSM")}>Digital Marketing Specialist</p>
+        <p className={cx("bodyRegularSM")}>{authorRole}</p>
       </div>
     </div>
   )
