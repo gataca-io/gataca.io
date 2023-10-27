05f538e8e3696c2c4d9a75a5a77fe6d75777d04e
Mayra Amador <127114451+mayra-gataca@users.noreply.github.com>
2023-10-27 09:19:56 +0200

    DEV-3317: Add Authors section on blog post (#251)
    * add author info in blog page
    
    * styles second section right
    
    * styles second section right adj
    
    * use Author component data
    
    * styles authors data
    
    * fix padding and margin container firstSection
    
    * styles container and media query
    
    * fix media query order
    
    * fix media query order
    
    * fix flex rule and iteration
    
    * comment css for yarn build
    
    * comment css for yarn build
    
    * get rid of comment css for yarn build
    
    * fix card size blog Preview
    
    * fix margin size with new info element
    
    * change link to a tag for target blank
    
    * blog author info updated
    
    * fix url and name
    
    * fix left side size
    
    * clean unnecessary code
    
    * add max witdh to left side

--- web/src/templates/blog/sections/secondSection/SecondSection.tsx
+++ web/src/templates/blog/sections/secondSection/SecondSection.tsx
@@ -1,14 +1,16 @@
 import * as React from "react"
 import cx from "classnames"
+import StrapiAuthor from "../../../../components/atoms/strapi/StrapiAuthor"
 import MarkDownContent from "../../../../components/elements/markDownContent/MarkDownContent"
 import * as styles from "./secondSection.module.scss"
 
 export type IContentProps = {
   content?: any
   className?: string
+  author: string
 }
 const SecondSection: React.FC<IContentProps> = props => {
-  const { content } = props
+  const { content, author } = props
 
   return (
     <section
@@ -19,6 +21,9 @@ const SecondSection: React.FC<IContentProps> = props => {
       <div className={`${styles.secondSection}`}>
         <div className={styles.secondSection__leftSide}>
           {content?.length && <MarkDownContent content={content} />}
+        </div>
+        <div className={styles.secondSection__rightSide}>
+          <StrapiAuthor author={author} />
         </div>
       </div>
     </section>
