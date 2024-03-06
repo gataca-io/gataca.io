7b4f8cd99830328cf43bb0b63d1106098d8baf34
Samuel Gómez <samuel@gataca.io>
2024-03-06 15:54:35 +0100

    AdFeature

--- admin/src/api/adv-feature/controllers/adv-feature.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * adv-feature controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::adv-feature.adv-feature');
--- admin/src/api/adv-feature/routes/adv-feature.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * adv-feature router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::adv-feature.adv-feature');
