5d70118f8c6404c1817f1250acae11701061dcd2
Samuel Gómez <samuel@gataca.io>
2024-03-07 22:50:40 +0100

    Wallet AdFeature

---
+++ admin/src/api/wallet-ad-feature/services/wallet-ad-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * wallet-ad-feature service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::wallet-ad-feature.wallet-ad-feature');
