bcaa3121a71d39be8201b5de5713ac3be452c670
ebpe <ebelmonte82@gmail.com>
2024-03-07 08:05:37 +0100

    Fixes in about us props type

--- web/src/pages/company/aboutUs/index.tsx
+++ web/src/pages/company/aboutUs/index.tsx
@@ -13,31 +13,31 @@ import FifthSection from "./sections/fifthSection/FifthSection"
 import SixthSection from "./sections/sixthSection/SixthSection"
 
 const AboutUsPage: React.FC<PageProps> = () => {
-  const [aboutUsData, setaboutUs] = useState<any | undefined>()
   const [strapiData, setStrapiData] = React.useState<any | undefined>()
   const {
-    firstSection,
-    secondSection,
-    thirdSection,
-    fourthSection,
-    fifthSection,
-    sixthSection,
-  } = aboutUsData ? aboutUsData : []
+    headerSection,
+    ourStorySection,
+    missionDescription,
+    valuesSection,
+    employeeSection,
+    awardsSection,
+    fundingDescription,
+  } = strapiData ? strapiData : []
 
   React.useEffect(() => {
     if (!strapiData) {
       getStrapiData()
     }
-    getaboutUs()
+    // getaboutUs()
   }, [])
-  const getaboutUs = async () => {
-    const json_data = require("./data/aboutUsData.json")
-    setaboutUs(json_data?.data && json_data?.data)
-  }
+  // const getaboutUs = async () => {
+  //   const json_data = require("./data/aboutUsData.json")
+  //   setaboutUs(json_data?.data && json_data?.data)
+  // }
 
   const getStrapiData = async () => {
     await fetch(
-      `${process.env.STRAPI_API_URL}/api/company-about-us?&populate=*`
+      `${process.env.STRAPI_API_URL}/api/company-about-us?&populate[headerSection][populate]=*&populate[ourStorySection][populate]=*&populate[employeeSection][populate]=*&populate[awardsSection][populate]=*&populate[valuesSection][populate]=*&populate[seo]=*`
     )
       .then(response => response.json())
       .then(jsonResponse => {
@@ -50,21 +50,21 @@ const AboutUsPage: React.FC<PageProps> = () => {
     <Layout seoData={strapiData?.seo}>
       <>
         <FirstSection
-          titleHeader={firstSection?.titleHeader}
-          descriptionHeader={firstSection?.descriptionHeader}
-          title={firstSection?.title}
-          descriptionParagraphs={firstSection?.descriptionParagraphs}
+          titleHeader={headerSection?.title}
+          descriptionHeader={headerSection?.subtitle}
+          title={ourStorySection?.title}
+          descriptionParagraphs={[ourStorySection?.firstParagraph, ourStorySection?.secondParagraph, ourStorySection?.thirdParagraph]}
         />
-        <SecondSection description={secondSection?.description} />
-        <ThirdSection title={thirdSection?.title} list={thirdSection?.list} />
+        <SecondSection description={missionDescription} />
+        <ThirdSection title={valuesSection?.title} list={valuesSection?.values.data} />
         <FourthSection
-          title={fourthSection?.title}
-          description={fourthSection?.description}
-          button={fourthSection?.button}
-          list={fourthSection?.list}
+          title={employeeSection?.title}
+          description={employeeSection?.description}
+          button={employeeSection?.button}
+          list={employeeSection?.employees.data}
         />
-        <FifthSection title={fifthSection?.title} list={fifthSection?.list} />
-        <SixthSection description={sixthSection?.description} />
+        <FifthSection title={awardsSection?.title} list={awardsSection?.awards.data} />
+        <SixthSection description={fundingDescription} />
         <PreFooterCTASection
           title={"Ready to start?"}
           description={
