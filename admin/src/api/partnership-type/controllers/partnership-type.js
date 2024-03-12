d148c67e10738647ca89436fe71db6eb09654d6f
Samuel Gómez <samuel@gataca.io>
2024-03-12 20:39:47 +0100

    Partneship type

---
+++ admin/src/api/partnership-type/controllers/partnership-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * partnership-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::partnership-type.partnership-type');
