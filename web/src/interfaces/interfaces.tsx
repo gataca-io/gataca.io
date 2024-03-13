ed248efab02ce4e11ca4fa797d3d889e9ddbc7fe
ebpe <ebelmonte82@gmail.com>
2024-03-13 07:51:49 +0100

    Use strapi data model to checkGreyPanel and productModel

--- web/src/interfaces/interfaces.tsx
+++ web/src/interfaces/interfaces.tsx
@@ -70,19 +70,20 @@ export interface StrapiImageModel {
 
 export interface IProductModel {
   id: string
-  attributes: any[]
-  name: string
-  description: string
-  prices: IPriceModel[]
-  type?: string
-  features?: IProductFeaturesModel
-  subPriceMonthLabel?: string
-  subPriceYearLabel?: string
+  attributes:  {
+    name: string
+    description: string
+    prices: IPriceModel[]
+    type?: string
+    features?: IProductFeaturesModel
+    subPriceMonthLabel?: string
+    subPriceYearLabel?: string
 
-  button: {
-    label: "Contact us"
-    outsideWeb: false
-    url: "/company/contact"
+    button: {
+      label: "Contact us"
+      outsideWeb: false
+      url: "/company/contact"
+    }
   }
 }
 
