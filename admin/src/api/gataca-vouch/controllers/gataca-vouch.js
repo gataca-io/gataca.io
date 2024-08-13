15932283fa0d36cd81b4490826b22cc6ef6d548c
Mayra <mayra@gataca.io>
2024-08-13 13:13:38 +0200

    feat[vouch]: new single type

---
+++ admin/src/api/gataca-vouch/controllers/gataca-vouch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gataca-vouch controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::gataca-vouch.gataca-vouch');
---
+++ admin/src/api/gataca-vouch/routes/gataca-vouch.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * gataca-vouch router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::gataca-vouch.gataca-vouch');
