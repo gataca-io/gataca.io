7ce9f24822d91f7b0925d7fb2d7a102f37172da4
Samuel Gómez <samuel@gataca.io>
2024-09-05 01:02:14 +0200

    Privacy Policy

--- web/src/data/privacyPolicyData.ts
+++ web/src/data/privacyPolicyData.ts
@@ -6,66 +6,60 @@ const tableOfContent: LinkModel[] = [
     label: "Table of content",
     subRoutes: [
       {
-        id: "whoAreWe",
-        label: "1. Who Are We",
-        route: "#whoAreWe__0",
+        id: "introduction",
+        label: "1. Introduction",
+        route: "#introduction__1",
       },
       {
         id: "definitions",
         label: "2. Definitions",
-        route: "#definitions__1",
+        route: "#definitions__2",
       },
       {
         id: "infoCollectionUSe",
         label: "3. Information, Collection and Use",
-        route: "#infoCollectionUSe__2",
+        route: "#infoCollectionUSe__3",
       },
       {
         id: "personalDataAccess",
-        label: "4. What personal data does GATACA access about You?",
-        route: "#personalDataAccess__3",
-      },
-      {
-        id: "thirdPartyServices",
         label:
-          "5. What Third Party Services do we use to collect and process your Data?",
-        route: "#thirdPartyServices__4",
+          "4. What Personal Data does Gataca access about Clients, Authorized Users and End Users?",
+        route: "#personalDataAccess__4",
       },
       {
-        id: "processingYourData",
+        id: "purposes",
         label:
-          "6. What are the purposes and legal basis for the processing of your data?",
-        route: "#processingYourData__5",
+          "5. What are the purposes and legal basis for the processing of your data?",
+        route: "#purposes__5",
       },
       {
-        id: "sharedData",
-        label: "7. With which recipients will your data be shared?",
-        route: "#sharedData__6",
+        id: "recipients",
+        label: "6. With which recipients will your data be shared?",
+        route: "#recipients__6",
       },
       {
         id: "transferofData",
-        label: "8. International transfer of Data",
+        label: "7. International transfer of Data",
         route: "#transferofData__7",
       },
       {
         id: "retentionData",
-        label: "9. Retention of Data",
+        label: "8. Retention of Data",
         route: "#retentionData__8",
       },
       {
         id: "gdpr",
-        label:
-          "10. Your Data Protection Rights under the General Data Protection Regulation (GDPR)",
+        label: "9. Your Data Protection Rights",
         route: "#gdpr__9",
       },
       {
         id: "changesPrivacyPolicy",
-        label: "11. Changes to this Privacy Policy",
+        label: "10. Changes to this Privacy Policy",
         route: "#changesPrivacyPolicy__10",
       },
       {
         id: "contact",
-        label: "12. Contact",
+        label: "11. Contact",
         route: "#contact__11",
       },
     ],
