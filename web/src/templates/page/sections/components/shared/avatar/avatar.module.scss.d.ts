3b9c1ed6a951a48a0aa4febd32f831b357679759
Mayra Amador <127114451+mayra-gataca@users.noreply.github.com>
2024-10-08 16:02:19 +0200

    DEV-4449: Components: Avatars (#314)
    * feat[Avatar]: new component & apply to Blog
    
    * Change to image version to 7.0.15-SNAPSHOT because of gataca.io CD DEV action.
    
    ---------
    
    Co-authored-by: mayra-gataca <mayra-gataca@gataca.io>

---
+++ web/src/templates/page/sections/components/shared/avatar/avatar.module.scss.d.ts
@@ -0,0 +1,7 @@
+export const avatarContainer: string
+export const mediaSizeXXL: string
+export const mediaSizeXL: string
+export const mediaSizeLG: string
+export const mediaSizeMD: string
+export const mediaSizeSM: string
+export const mediaSizeXS: string
