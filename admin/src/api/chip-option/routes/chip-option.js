5aa3388a732cb32f24679841579ebcc95fb29dbc
Mayra <mayra@gataca.io>
2024-10-17 12:56:20 +0200

    feat[chip]: new components & relations

---
+++ admin/src/api/chip-option/routes/chip-option.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * chip-option router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::chip-option.chip-option');
---
+++ web/src/templates/page/sections/components/generic/chip/Chip.tsx
@@ -0,0 +1,80 @@
+import * as React from "react"
+import cx from "classnames"
+import * as styles from "./chip.module.scss"
+import { ChipModel } from "../../../../../../interfaces/interfaces"
+import StrapiImage from "../../../../../../components/atoms/images/StrapiImage"
+
+const Chip: React.FC<ChipModel> = props => {
+  const {
+    idChip,
+    text,
+    action,
+    type,
+    form,
+    color,
+    leadingIcon,
+    trailingIcon,
+    chipSize,
+    disabled,
+  } = props
+
+  const chipStyles: Record<string, string> = {
+    outline: styles?.outlineChip,
+    text: styles?.text,
+  }
+
+  const chipForm: Record<string, string> = {
+    round: styles?.roundChip,
+  }
+
+  const sizeStyles: Record<string, string> = {
+    xsmall: "bodyRegularXS",
+    small: "buttonSM",
+  }
+
+  const colorStyles: Record<string, string> = {
+    red: styles?.redColor,
+    green: styles?.greenColor,
+    purple: styles?.purpleColor,
+    yellow: styles?.yellowColor,
+    blue: styles?.blueColor,
+    grey: styles?.greyColor,
+    teal: styles?.tealColor,
+    pink: styles?.pinkColor,
+    black: styles?.blackColor,
+    white: styles?.chipStrapi,
+  }
+
+  return (
+    <>
+      <button
+        id={idChip}
+        className={`
+            ${type ? chipStyles[type] : styles?.chipStrapi} ${
+          chipSize ? sizeStyles[chipSize] : styles?.chipStrapi
+        } ${color ? colorStyles[color] : styles?.chipStrapi} ${
+          styles.chipStrapi
+        } ${form ? chipForm[form] : styles?.chipStrapi} ${
+          disabled ? styles.disabled : styles?.chipStrapi
+        } ${styles.chipStrapi}`}
+        onClick={action}
+      >
+        {leadingIcon ? (
+          <StrapiImage image={leadingIcon ? leadingIcon : null} />
+        ) : null}
+        {text ? (
+          <span
+            className={cx(chipSize ? sizeStyles[chipSize] : "bodyRegularXS")}
+          >
+            {text}
+          </span>
+        ) : null}
+        {trailingIcon ? (
+          <StrapiImage image={trailingIcon ? trailingIcon : null} />
+        ) : null}
+      </button>
+    </>
+  )
+}
+
+export default Chip
