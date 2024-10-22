a467d8464f68294bf9d1f618e520b1934be2e840
Mayra <mayra@gataca.io>
2024-10-22 16:57:51 +0200

    feat[list]: new component & single type

---
+++ admin/src/api/list-option/routes/list-option.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * list-option router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::list-option.list-option');
