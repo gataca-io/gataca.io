368b04fcf87e7e93398464d9478e98dcd219bc71
Mayra <mayra@gataca.io>
2024-11-04 11:44:56 +0100

    feat[SegmentedButtonsContainer]: refactor code after return

--- web/src/templates/page/sections/components/generic/segmentedButtons/SegmentedButtonsContainer.tsx
+++ web/src/templates/page/sections/components/generic/segmentedButtons/SegmentedButtonsContainer.tsx
@@ -16,62 +16,52 @@ const SegmentedButtonsContainer: React.FC<
   const [openItem, setOpenItem] = React.useState<number>(1)
 
   return (
-    <>
-      <div
-        className={`${styles?.segmentedButtons__container} ${
-          className && className
-        }`}
-      >
-        <div className={styles?.segmentedOptions}>
-          {segmentedOptions?.map((item: any, index: number) => {
-            const { button } = item.attributes
+    <div
+      className={`${styles?.segmentedButtons__container} ${
+        className && className
+      }`}
+    >
+      <div className={styles?.segmentedOptions}>
+        {segmentedOptions?.map((item: any, index: number) => {
+          const { button } = item.attributes
 
-            return (
-              <SegmentedButtons
-                id={"segmentedButton__" + index}
-                key={"segmentedButton__" + index}
-                index={index + 1}
-                label={button?.label}
-                color={button?.color}
-                size={button?.size}
-                noPaddingText={button?.noPaddingText}
-                disabled={button?.disabled}
-                selected={openItem === index + 1}
-                showItem={index => {
-                  const element =
-                    document &&
-                    document?.getElementById("listCategory__" + (index - 1))
-                  setOpenItem(index), element
-                }}
-              />
-            )
-          })}
-        </div>
-        <>
-          {segmentedOptions[0]?.attributes?.content?.length ? (
-            <div key={"contentContainer__" + Math.random()}>
-              {segmentedOptions?.map((item: any, index: number) => {
-                const { content } = item.attributes
-
-                return (
-                  <>
-                    {openItem === index + 1 ? (
-                      <>
-                        <div key={"content__" + index}>
-                          {content?.length && (
-                            <MarkDownContent content={content} />
-                          )}
-                        </div>
-                      </>
-                    ) : null}
-                  </>
-                )
-              })}
-            </div>
-          ) : null}
-        </>
+          return (
+            <SegmentedButtons
+              id={"segmentedButton__" + index}
+              key={"segmentedButton__" + index}
+              index={index + 1}
+              label={button?.label}
+              color={button?.color}
+              size={button?.size}
+              noPaddingText={button?.noPaddingText}
+              disabled={button?.disabled}
+              selected={openItem === index + 1}
+              showItem={index => {
+                const element =
+                  document &&
+                  document?.getElementById("listCategory__" + (index - 1))
+                setOpenItem(index), element
+              }}
+            />
+          )
+        })}
       </div>
-    </>
+      <>
+        {segmentedOptions[0]?.attributes?.content?.length ? (
+          <div>
+            {segmentedOptions?.map((item: any, index: number) => {
+              const { content } = item.attributes
+
+              return openItem === index + 1 ? (
+                <div key={"content__" + index}>
+                  {content?.length && <MarkDownContent content={content} />}
+                </div>
+              ) : null
+            })}
+          </div>
+        ) : null}
+      </>
+    </div>
   )
 }
 
