25e7ac8965b3b7c7d1b63d8395220ae664820c0b
Mayra <mayra@gataca.io>
2024-11-07 15:00:36 +0100

    feat[Video]: add condition to show heading container

--- web/src/templates/page/sections/components/shared/video/Video.tsx
+++ web/src/templates/page/sections/components/shared/video/Video.tsx
@@ -77,47 +77,49 @@ const Video: React.FC<ISectionProps> = props => {
       id="homeVideoSection"
       className={`${styles?.video} ${cx("containerMaxWidth")}`}
     >
-      <div className={`${styles?.video__heading} ${className && className}`}>
-        <Heading
-          idHeading={heading?.idHeading}
-          titleSize={heading?.titleSize}
-          align={heading?.align}
-          extraText={heading?.extraText}
-          title={heading?.title}
-          sectionName={heading?.sectionName}
-          content={heading?.content}
-          buttonGroup={heading?.buttonGroup?.buttons?.data}
-          list={heading?.list?.list_options?.data}
-          segmentedButton={heading?.segmentedButton?.buttons?.data}
-          table={heading?.table?.content}
-          chip={{
-            idChip: heading?.chip?.idChip,
-            text: heading?.chip?.text,
-            type: heading?.chip?.type,
-            form: heading?.chip?.form,
-            disabled: heading?.chip?.disabled,
-            color: heading?.chip?.color,
-            chipSize: heading?.chip?.chipSize,
-            leadingIcon: heading?.chip?.leadingIcon,
-            trailingIcon: heading?.chip?.trailingIcon,
-          }}
-          button={{
-            idButton: heading?.button?.idButton,
-            label: heading?.button?.label,
-            icon: heading?.button?.icon,
-            style: heading?.button?.style,
-            color: heading?.button?.color,
-            size: heading?.button?.size,
-            noPaddingText: heading?.button?.noPaddingText,
-            disabled: heading?.button?.disabled,
-            link: heading?.button?.link,
-            url: heading?.button?.url,
-            outsideWeb: heading?.button?.outsideWeb,
-            action: () => window.open(heading?.button?.url, "_blank"),
-          }}
-          key={`heading_` + Math.random()}
-        />
-      </div>
+      {heading && (
+        <div className={`${styles?.video__heading} ${className && className}`}>
+          <Heading
+            idHeading={heading?.idHeading}
+            titleSize={heading?.titleSize}
+            align={heading?.align}
+            extraText={heading?.extraText}
+            title={heading?.title}
+            sectionName={heading?.sectionName}
+            content={heading?.content}
+            buttonGroup={heading?.buttonGroup?.buttons?.data}
+            list={heading?.list?.list_options?.data}
+            segmentedButton={heading?.segmentedButton?.buttons?.data}
+            table={heading?.table?.content}
+            chip={{
+              idChip: heading?.chip?.idChip,
+              text: heading?.chip?.text,
+              type: heading?.chip?.type,
+              form: heading?.chip?.form,
+              disabled: heading?.chip?.disabled,
+              color: heading?.chip?.color,
+              chipSize: heading?.chip?.chipSize,
+              leadingIcon: heading?.chip?.leadingIcon,
+              trailingIcon: heading?.chip?.trailingIcon,
+            }}
+            button={{
+              idButton: heading?.button?.idButton,
+              label: heading?.button?.label,
+              icon: heading?.button?.icon,
+              style: heading?.button?.style,
+              color: heading?.button?.color,
+              size: heading?.button?.size,
+              noPaddingText: heading?.button?.noPaddingText,
+              disabled: heading?.button?.disabled,
+              link: heading?.button?.link,
+              url: heading?.button?.url,
+              outsideWeb: heading?.button?.outsideWeb,
+              action: () => window.open(heading?.button?.url, "_blank"),
+            }}
+            key={`heading_` + Math.random()}
+          />
+        </div>
+      )}
       <div
         className={`${styles.video__videoContainer} ${
           color ? colorStyles[color] : styles?.blackBackground
