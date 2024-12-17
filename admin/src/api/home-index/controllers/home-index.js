51c81c804875c489ff215b014cca59c0794aacc8
Mayra <mayra@gataca.io>
2024-12-17 13:52:19 +0100

    feat[HomeIndex]: we use singleType instead of Pages
    in another PR we will replace this singleType data for index file

---
+++ admin/src/api/home-index/controllers/home-index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-index controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-index.home-index');
---
+++ admin/src/api/home-index/routes/home-index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-index router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-index.home-index');
