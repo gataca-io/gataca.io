1761ad8332001b33ecb371ba9fdbb4b10e1f80f1
Mayra Amador <127114451+mayra-gataca@users.noreply.github.com>
2025-01-23 17:54:18 +0100

    DEV-4672: Home page- change api & change naming of strapified pages in PRO (#356)
    * feat[index]: use homeIndex data from singleTypes
    
    * feat[pages]: change naming to pages made with Strapi
    
    * feat[carrrouselLogos]: fix color naming & choosing
    
    * feat[package]: update version
    
    * Change to image version to 7.1.23-SNAPSHOT because of gataca.io CD DEV action.
    
    * feat[DynamicSelecto]: when top left adjust to right height
    
    ---------
    
    Co-authored-by: mayra-gataca <mayra-gataca@gataca.io>

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,57 +1,19 @@
-import { PageProps, navigate } from "gatsby"
 import * as React from "react"
-import { useState } from "react"
 import Layout from "../components/templates/mainLayout/MainLayout"
-import PreFooterCTASection from "../components/templates/sections/preFooterCTA/PreFooterCTA"
-import LogosSlider from "../components/elements/logosSlider/LogosSlider"
-import EighthSection from "./home/sections/eighthSection/EighthSection"
-import SixthSection from "./home/sections/sixthSection/SixthSection"
-import FifthSection from "./home/sections/fifthSection/FifthSection"
-import FourthSection from "./home/sections/fourthSection/FourthSection"
-import ThirdSection from "./home/sections/thirdSection/ThirdSection"
-import FirstSection from "./home/sections/firstSection/FirstSection"
-import * as styles from "./home/home.module.scss"
-import { sortByDate } from "../utils/sort"
-import { demolandURL, gatacaStudioURL } from "../data/globalData"
-import SeventhSection from "./home/sections/seventhSection/SeventhSection"
+import Highlight from "../templates/page/sections/components/shared/Highlight/Highlight"
+import AllSectionsTemplate from "../templates/page/sections/AllSectionsTemplate"
+import PageSkeleton from "../templates/page/components/introBlogSkeleton/PageSkeleton"
 
-const IndexPage: React.FC<PageProps> = () => {
-  const [blogsItems, setBlogsItems] = React.useState<any | undefined>()
-  const [homeStrapiData, setHomeStrapiData] = useState<any | undefined>()
-
-  const {
-    firstSection,
-    thirdSection,
-    fourthSection,
-    fifthSection,
-    sixthSection,
-    seventhSection,
-    eighthSection,
-  } = homeStrapiData ? homeStrapiData : []
+const IndexPage: React.FC = (props: any) => {
+  const [homeStrapiData, setHomeStrapiData] = React.useState<any | undefined>()
+  const homeData = homeStrapiData && homeStrapiData
 
   React.useEffect(() => {
     getHomeStrapiData()
-    if (!blogsItems) {
-      getBlogsData()
-    }
   }, [])
 
-  const getBlogsData = async () => {
-    await fetch(
-      `${process.env.STRAPI_API_URL}/api/blogs?sort[0]=date%3Aasc&populate=*`
-    )
-      .then(response => response.json())
-      .then(jsonResponse => {
-        const blogs = jsonResponse?.data
-        const sorteredBlogs = sortByDate(blogs)
-        setBlogsItems(sorteredBlogs)
-      })
-  }
-
   const getHomeStrapiData = async () => {
-    await fetch(
-      `${process.env.STRAPI_API_URL}/api/entry?&populate[seo][populate]=*&populate[firstSection][populate]=*&populate[thirdSection][populate]=*&populate[fourthSection][populate]=*&populate[fifthSection][populate]=*&populate[sixthSection][populate]=*&populate[seventhSection][populate]=*&populate[eighthSection][populate]=*&populate[localizations][populate]=*`
-    )
+    await fetch(`${process.env.STRAPI_API_URL}/api/home-index?populate=deep,7`)
       .then(response => response.json())
       .then(jsonResponse => {
         const homeStrapiData = jsonResponse?.data?.attributes
@@ -60,66 +22,32 @@ const IndexPage: React.FC<PageProps> = () => {
   }
 
   return (
-    <Layout seoData={homeStrapiData?.seo}>
+    <Layout seoData={homeData?.seo}>
       <>
-        <FirstSection
-          title={firstSection?.title}
-          description={firstSection?.description}
-          leftButton={firstSection?.leftButton}
-          rightButton={firstSection?.rightButton}
-        />
-        <LogosSlider />
-        <ThirdSection
-          title={thirdSection?.title}
-          description={thirdSection?.description}
-          list={thirdSection?.useCases}
-        />
-        <FourthSection
-          title={fourthSection?.title}
-          description={fourthSection?.description}
-          advantages={fourthSection?.advantages}
-        />
-        <FifthSection
-          title={fifthSection?.title}
-          subTitle={fifthSection?.subtitle}
-          description={fifthSection?.description}
-        />
-        <SixthSection
-          title={sixthSection?.title}
-          subTitle={sixthSection?.subtitle}
-          description={sixthSection?.description}
-          list={sixthSection?.useCases}
-          learnMoreButton={sixthSection?.learnMoreButton}
-        />
-        <SeventhSection
-          title={seventhSection?.title}
-          subTitle={seventhSection?.subtitle}
-          description={seventhSection?.description}
-          feedback={seventhSection?.feedbacks}
-        />
-        <EighthSection
-          title={eighthSection?.title}
-          description={eighthSection?.description}
-          blogs={blogsItems}
-          moreButton={eighthSection?.moreButton}
-        />
-        <PreFooterCTASection
-          className={styles.prefooter}
-          title={"Ready to start?"}
-          description={
-            "Create an account in Gataca Studio and start experiencing decentralized identity today. Fast & seamless integration"
-          }
-          leftButton={{
-            label: "Contact us",
-            style: "outline",
-            color: "white",
-            action: () => navigate("/company/contact"),
-          }}
-          rightButton={{
-            label: "Try Demo",
-            action: () => window.open(demolandURL, "_blank"),
-          }}
-        />
+        {homeData ? (
+          <AllSectionsTemplate
+            {...homeStrapiData}
+            location={props?.location?.hash}
+            pageContext={props?.pageContext}
+          />
+        ) : (
+          <PageSkeleton />
+        )}
+        {homeData?.sections?.map((item: any) => {
+          const { __component, idItem, heading, color, align } = item
+
+          return (
+            __component === "shared.highlight" && (
+              <Highlight
+                key={"highlight__" + Math.random()}
+                idItem={idItem}
+                heading={heading?.data?.attributes}
+                color={color}
+                align={align}
+              />
+            )
+          )
+        })}
       </>
     </Layout>
   )
